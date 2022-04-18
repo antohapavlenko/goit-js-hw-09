b2a710f373f11945509a99fde6a1f14c4b48a945
Anton Pavlenko <86660391+antohapavlenko@users.noreply.github.com>
2022-04-18 13:44:35 +0300

    3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', getFormValue)
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const shouldResolve = Math.random() > 0.3;
+            if (shouldResolve) {
+                resolve({ position, delay });
+                
+            } else {
+                reject({ position, delay });
+            };
+        }, delay);  
+    });
+};
+
+
+function getFormValue(event) {
+    event.preventDefault();
+
+    let inputValue = {};
+    const formData = new FormData(form)
+    formData.forEach((value, key) => inputValue[key] = value);
+
+    const delay = Number(inputValue.delay);
+    const step = Number(inputValue.step);
+    const amount = Number(inputValue.amount);
+    
+
+    for (let i = 0; i < amount; i += 1) {
+        createPromise(i, delay + step * i)
+        .then(({ position, delay }) => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { timeout: 5000 });
+        })
+        .catch(({ position, delay }) => {
+            Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { timeout: 5000 });
+        }); 
+    }
+};
+ 
